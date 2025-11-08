25725583905c677790604d0c1c1571a2653e2cf2
camille <camille.gravier@epfedu.fr>
2025-11-08 12:11:33 +0100

    suppresion de fichiers inutile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component'; 
-import { HomepagComponent } from './homepag/homepag.component';
 import { HomePageComponent} from './home-page/home-page.component';
 import { RecetteComponent } from './recette/recette.component';
 import { IngredientsComponent } from './ingredients/ingredients.component';
@@ -9,7 +8,6 @@ import { AdminPageComponent } from './admin-page/admin-page.component';
 
 export const routes: Routes = [{ path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'homepag', component: HomepagComponent },
   { path: 'homepage', component: HomePageComponent },
   { path: 'admin', component: AdminPageComponent },
   { path: 'recette/:id', component: RecetteComponent },
