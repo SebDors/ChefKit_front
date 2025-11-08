fb599b2a6e71ca3369780d91760bd227d81bda8d
Sebastien Dors <dorssebastien@gmail.com>
2025-11-08 09:36:43 +0100

    Affichage des ingrédients possédés et manquants

--- src/app/recette/recette.component.ts
+++ src/app/recette/recette.component.ts
@@ -5,6 +5,7 @@ import { RecetteService } from '../services/recette.service';
 import { Recette } from '../models/recette.model';
 import { AuthService } from '../auth.service';
 import { Ingredient } from '../models/ingredient.model';
+import { IngredientsService } from '../services/ingredients.service';
 
 @Component({
   selector: 'app-recette',
@@ -19,31 +20,55 @@ export class RecetteComponent implements OnInit {
   isLoading = true;
   isScrolled = false;
   currentYear: number = new Date().getFullYear();
+  
   ingredients: Ingredient[] = [];
+
+  ownedIngredients: Ingredient[] = [];
+  missingIngredients: Ingredient[] = [];
+  isUserLoggedIn = false;
 
   constructor(
     private route: ActivatedRoute,
     private recetteService: RecetteService,
     private router: Router,
     public authService: AuthService,
-    private location: Location
+    private location: Location,
+    private ingredientsService: IngredientsService
   ) {}
 
   ngOnInit(): void {
+    this.isUserLoggedIn = this.authService.isLoggedIn();
     const id = Number(this.route.snapshot.paramMap.get('id'));
+
     if (id) {
       this.recetteService.getRecetteById(id).subscribe({
         next: (res) => {
           this.recette = res;
           if (this.recette && this.recette.instructions) {
             this.instructionSteps = this.recette.instructions
-  .split(/\s*\d+\.\s*|\.\s*(?=[A-ZÉÀÈÙÛÂÊÎÔUMJ])/) // Divise par "numéro." ou ". " (suivi d'une majuscule)
-  .filter(step => step.trim() !== '');
+              .split(/\s*\d+\.\s*|\.\s*(?=[A-ZÉÀÈÙÛÂÊÎÔUMJ])/)
+              .filter(step => step.trim() !== '');
           }
+
           this.recetteService.getIngredientsByRecetteId(id).subscribe({
             next: (ingredientsRes) => {
               this.ingredients = ingredientsRes;
-              this.isLoading = false;
+
+              if (this.isUserLoggedIn) {
+                const currentUser = this.authService.currentUser();
+                if (currentUser) {
+                  this.ingredientsService.getFridgeIngredients(currentUser.nomUtilisateur).subscribe(fridgeIngredients => {
+                    const fridgeIngredientNames = fridgeIngredients.map(ing => ing.nomIngredient);
+                    this.ownedIngredients = this.ingredients.filter(recipeIng => fridgeIngredientNames.includes(recipeIng.name));
+                    this.missingIngredients = this.ingredients.filter(recipeIng => !fridgeIngredientNames.includes(recipeIng.name));
+                    this.isLoading = false;
+                  });
+                } else {
+                  this.isLoading = false;
+                }
+              } else {
+                this.isLoading = false;
+              }
             },
             error: (err) => {
               console.error('Erreur lors de la récupération des ingrédients:', err);
